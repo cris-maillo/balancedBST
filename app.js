d7b9ed3f5d6dd76b117abea024675d53436f83f5
Cris <cristinamaillo.r@gmail.com>
2023-05-13 14:20:39 +0100

    added print tree to class

--- app.js
+++ app.js
@@ -32,6 +32,19 @@ class Tree{
 
         return node;
     }
+
+    printTree(node, prefix = '', isLeft = true){
+        if (node === null) {
+           return;
+        }
+        if (node.right !== null) {
+            this.printTree(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        if (node.left !== null) {
+            this.printTree(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
+    }
 }
 
 
@@ -65,21 +78,8 @@ function removeDuplicates(arr){
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 let tree = new Tree()
-
 let root = tree.buildTree(arr)
+tree.printTree(root);
 
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node === null) {
-       return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
-}
 
-prettyPrint(root)
